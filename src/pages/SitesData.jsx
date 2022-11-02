19ace1e370dd6eb4b6119eb580d778992f43d14f
kushtriimhyseni <kushtrim@starlabspro.com>
2022-11-02 21:19:52 +0100

    Update SitesData.jsx

--- src/pages/SitesData.jsx
+++ src/pages/SitesData.jsx
@@ -17,7 +17,11 @@ const SitesData = () => {
   }, [url, checkPageSpeed]);
 
   if (loading) {
-    return <div>{loading && <Spinner />}</div>;
+    return (
+      <div>
+        <Spinner />
+      </div>
+    );
   } else {
     return (
       <div>
